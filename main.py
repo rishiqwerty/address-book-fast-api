d7b0af7ce5cf8d3fdf8d700e3ff44b026f796922
Rishav Sharam <rishav.sharma.18@mountblue.tech>
2022-11-28 23:36:06 +0530

    Adding update and delete operations

--- main.py
+++ main.py
@@ -2,19 +2,12 @@ from fastapi import FastAPI
 from pydantic import BaseModel, Field
 app = FastAPI()
 
-a = {
-    'Name': 'Unknown',
-    'Address': 'Mango',
-    'Latitude': 23,
-    'Longitude': 32
-}
-
 class Address(BaseModel):
     id: int
     user: str = Field(min_length=1)
     address: str = Field(min_length=10)
-    latitude: int
-    longitude: int
+    latitude: int = Field(Lt=1000)
+    longitude: int = Field(Lt=1000)
 
 ADDRESSES = []
 
@@ -26,3 +19,17 @@ async def test():
 async def create_address(address: Address):
     ADDRESSES.append(address)
     return address
+
+@app.put('/{address_id}')
+async def update_address(address_id: int, address: Address):
+
+    for i in ADDRESSES:
+        if address_id == i.id:
+            ADDRESSES[ADDRESSES.index(i)] = address
+            return ADDRESSES[ADDRESSES.index(i)]
+@app.delete('/{address_id}')
+async def delete_address(address_id: int):
+    for x in ADDRESSES:
+        if address_id == x.id:
+            del ADDRESSES[ADDRESSES.index(x)]
+            return f'id: {address_id} deleted'
